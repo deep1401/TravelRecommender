dbb6608feb1263f35ec3bc11580bdd8f83e44fc4
deep1401 <gandhi0869@gmail.com>
2020-05-15 10:40:04 +0530

    Telegram bot

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ from apikeys import apikey
 from flask_cors import CORS, cross_origin
 import pandas as pd
 from sklearn.cluster import MiniBatchKMeans
+from telegram.bot import Bot
+from telebot.credentials import bot_token, bot_user_name, URL
+from telebot.mastermind import get_response
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 app.config["DEBUG"] = True
 secret_key = apikey()
+
+global bot
+global TOKEN
+TOKEN = bot_token
+bot = Bot(token=TOKEN)
 
 
 def get_location_attributes(location):
@@ -29,14 +37,12 @@ def fit_and_inference(dataframe, number_of_days):
         df = dataframe[['latitude', 'longitude']]
         df_matrix = df.values
         model.fit(df_matrix)
-        predicted_labels=model.predict(df_matrix)
+        predicted_labels = model.predict(df_matrix)
 
     else:
-        predicted_labels= [0]*len(dataframe)
+        predicted_labels = [0] * len(dataframe)
 
     return predicted_labels + 1
-
-
 
 
 @app.route('/')
@@ -64,10 +70,10 @@ def generate_itinerary():
                     tourist_dict['latitude'].append(tourist_data['results']['items'][i]['position'][0])
                     tourist_dict['longitude'].append(tourist_data['results']['items'][i]['position'][1])
                 tourist_df = pd.DataFrame(tourist_dict)
-                day_of_travel= fit_and_inference(tourist_df,number_of_days)
+                day_of_travel = fit_and_inference(tourist_df, number_of_days)
                 tourist_df['day_of_travel'] = day_of_travel
                 print(tourist_df)
-                return {'title': list(tourist_df['title']),'latitude': list(tourist_df['latitude']),
+                return {'title': list(tourist_df['title']), 'latitude': list(tourist_df['latitude']),
                         'longitude': list(tourist_df['longitude']), 'day_of_travel': list(tourist_df['day_of_travel'])}
 
             else:
@@ -77,6 +83,72 @@ def generate_itinerary():
             return jsonify(['Error fetching API'])
 
 
+@app.route(f'/{TOKEN}', methods=['POST'])
+def respond():
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    # get the chat_id to be able to respond to the same user
+    chat_id = update.message.chat.id
+    # get the message id to be able to reply to this specific message
+    msg_id = update.message.message_id
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = update.message.text.encode('utf-8').decode()
+    print("got text message :", text)
+    # here we call our super AI
+    response = get_response(text)
+    if type(response) != type(dict({})):
+        bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+        return 'OK'
+    else:
+        response_final = get_location_attributes(response['location'])
+        number_of_days = int(response['days'])
+        if type(response_final) != type(dict({})):
+            bot.sendMessage(chat_id=chat_id, text=str(response_final), reply_to_message_id=msg_id)
+            return 'OK'
+        else:
+            coordinate_dict = response_final['Response']['View'][0]['Result'][0]['Location']["NavigationPosition"][0]
+            latitude, longitude = coordinate_dict['Latitude'], coordinate_dict['Longitude']
+            location_response = requests.get(
+                f'https://places.ls.hereapi.com/places/v1/discover/search?at={latitude},{longitude}&r=100000&q'
+                f'=Landmark/Attraction&apiKey={secret_key}')
+            if location_response.status_code == 200:
+                if len(location_response.json()['results']['items']) > 0:
+                    tourist_data = location_response.json()
+                    tourist_dict = {'title': [], 'latitude': [], 'longitude': []}
+                    for i in range(len(tourist_data['results']['items'])):
+                        tourist_dict['title'].append(tourist_data['results']['items'][i]['title'])
+                        tourist_dict['latitude'].append(tourist_data['results']['items'][i]['position'][0])
+                        tourist_dict['longitude'].append(tourist_data['results']['items'][i]['position'][1])
+                    tourist_df = pd.DataFrame(tourist_dict)
+                    day_of_travel = fit_and_inference(tourist_df, number_of_days)
+                    tourist_df['day_of_travel'] = day_of_travel
+                    # print(tourist_df)
+                    response = str({'title': list(tourist_df['title']), 'latitude': list(tourist_df['latitude']),
+                                    'longitude': list(tourist_df['longitude']),
+                                    'day_of_travel': list(tourist_df['day_of_travel'])})
+                    bot.sendMessage(chat_id=chat_id, text=str(response), reply_to_message_id=msg_id)
+                    return 'OK'
+
+                else:
+                    bot.sendMessage(chat_id=chat_id, text='Nothing to roam here captain!', reply_to_message_id=msg_id)
+                    return 'OK'
+
+            else:
+                bot.sendMessage(chat_id=chat_id, text='Sorry we faced an error in API', reply_to_message_id=msg_id)
+                return 'OK'
+
+
+@app.route('/setwebhook', methods=['GET', 'POST'])
+def set_webhook():
+    # we use the bot object to link the bot to our app which live
+    # in the link provided by URL
+    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    # something to let us know things work
+    if s:
+        return "Webhook setup ok"
+    else:
+        return "Webhook setup failed"
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True)
---
+++ telebot/mastermind.py
@@ -0,0 +1,11 @@
+def get_response(msg):
+    if 'location' and 'days' not in msg:
+        return 'Please enter your desired destination and number of days in the following format: \n ' \
+               'location:<location> days:<number of days>'
+    else:
+        start = msg.find("location:") + len("location:")
+        end = msg.find("days")
+        substring = msg[start:end]
+        start1 = msg.find("days:") + len("days:")
+        substring1 = msg[start1:]
+        return {'location': substring.strip(), 'days': substring1.strip()}
